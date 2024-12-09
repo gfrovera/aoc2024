8612ca886c433bacf1b6aea7d5435bf8ebb12411
Vallkillmore <gfrovera@gmail.com>
2024-12-08 20:12:19 -0800

    day 1 part 1 solve. Whoohoo!

--- day00/part_01.py
+++ day00/part_01.py
@@ -1,8 +1,8 @@
 
-
-
 def input_splitter(path: str) -> list[list[int], list[int]]:
-
+    '''Takes the challenge input and creates a list of lists.
+    Each "column" in txt file becomes a list in the list of lists
+    '''
     result_lst = [[],[]]
 
     with open(path, 'r') as input:
@@ -12,22 +12,23 @@ def input_splitter(path: str) -> list[list[int], list[int]]:
 
             result_lst[0].append(l0)
             result_lst[1].append(l1)
+    
     return result_lst
 
-def pop_sorter(input: list[list[int], list[int]]) -> list[int]:
-    ...
+def pop_sorter(input: list[list[int], list[int]]) -> int:
+    '''Sorts lists in list of lists, converts all differences
+    of list values into the absolute values and finally sums all
+    items in list.
+    '''
+    l = sorted(input[0])
+    r = sorted(input[1])
+
+    diffs = [lv - rv for lv, rv in zip(l, r)]
+    abs_list = list(map(abs, y))
+
+    return sum(abs_list)
+    
 
 if __name__ == "__main__":
-    x = input_splitter('part_01_input.txt')
-
-    print(f'first 10 of list 0 are: {x[0][:10]}')
-    print(f'first 10 of list 1 are: {x[1][:10]}')
-
-    # Sort list
-    sx_one = sorted(x[0])
-    sx_two = sorted(x[1])
-
-    print(f'first 10 of list 0 are: {sx_one[:10]}')
-    print(f'first 10 of list 1 are: {sx_two[:10]}') 
-
-    print(enumerate(sx_one))
+    x = input_splitter('./part_01_input.txt')
+    pop_sorter(x)
